0ebe7ce8038132c4b78d99b44ec2275ef208924f
mahdi0shamlou <shamlouprogramer@gmail.com>
2024-08-11 13:29:02 -0400

    create a data fetch class in post sender service

--- POST_SENDER.py
+++ POST_SENDER.py
@@ -3,6 +3,19 @@ from DataBase_manager import *
 import time
 
 
+class DataFetcher:
+    def __init__(self, url):
+        self.url = url
+        # self.data = data
+
+    def fetch_json_data(self, data):
+        response = requests.post(self.url, data=data)
+        print(f'\t {response.status_code}')
+        response_json = response.json()
+        return response_json
+
+
+
 if __name__ == "__main__":
 
     pass
