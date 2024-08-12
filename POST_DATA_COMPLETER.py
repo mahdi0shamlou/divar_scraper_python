13b24ffcf2a3721a45a1001aa2ab461a3c9b4d2b
mahdi0shamlou <shamlouprogramer@gmail.com>
2024-08-12 15:08:55 -0400

    update get data from posts in data compeltetr

--- POST_DATA_COMPLETER.py
+++ POST_DATA_COMPLETER.py
@@ -1,17 +1,20 @@
-from DataBase_manager import *
+import sqlite3
 
 
 class GetData:
     """
         this class get all data of one token
     """
-    def __init__(self, token: str, db_obj: DatabaseManager):
+    def __init__(self, token: str, data_base_connection: sqlite3.connect):
         self.Token = token
-        self.Db_Obj = db_obj
+        self.DB_Conn = data_base_connection
         self.Data = []
 
     def _get_from_posts(self):
-        pass
+        cursor = self.DB_Conn.cursor()
+        cursor.execute('SELECT * FROM posts WHERE token = ?', (self.Token,))
+        posts = cursor.fetchall()
+        return posts
 
     def _get_from_posts_details(self):
         pass
@@ -20,9 +23,12 @@ class GetData:
         pass
 
     def get_data(self):
+        print(self._get_from_posts())
         return self.Data
 
 
 if __name__ == '__main__':
-    get_data_obj = GetData('token')
+    DATABASE = 'posts.db'
+    CONNECTION_DB = sqlite3.connect(DATABASE)
+    get_data_obj = GetData('gZnCrm6p', CONNECTION_DB)
     data = get_data_obj.get_data()
