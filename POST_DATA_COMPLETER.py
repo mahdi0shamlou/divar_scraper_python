7d46354d0d55f3510ba4d74604758835881fd9ac
mahdi0shamlou <shamlouprogramer@gmail.com>
2024-08-12 15:19:18 -0400

    GetData from one token in data compeleter is finished

--- POST_DATA_COMPLETER.py
+++ POST_DATA_COMPLETER.py
@@ -14,19 +14,38 @@ class GetData:
         cursor = self.DB_Conn.cursor()
         cursor.execute('SELECT * FROM posts WHERE token = ?', (self.Token,))
         posts = cursor.fetchall()
+        self.Data.append(posts[0][1]) # Token
+        self.Data.append(posts[0][2]) # City
+        self.Data.append(posts[0][3]) # titile
+        self.Data.append(posts[0][4]) # addres
+        self.Data.append(posts[0][5]) # image
+        self.Data.append(posts[0][6]) # time inserted
+        self.Data.append(posts[0][7]) # price
         return posts
 
     def _get_from_posts_details(self):
-        pass
+        cursor = self.DB_Conn.cursor()
+        cursor.execute('SELECT * FROM posts_details_personal WHERE token = ?', (self.Token,))
+        posts = cursor.fetchall()
+        self.Data.append(posts[0][2])  # desck
 
     def _get_from_personal_number(self):
-        pass
+        cursor = self.DB_Conn.cursor()
+        cursor.execute('SELECT * FROM personal_number WHERE token = ?', (self.Token,))
+        posts = cursor.fetchall()
+        self.Data.append(posts[0][3])  # phone number
 
     def get_data(self):
-        print(self._get_from_posts())
+        self._get_from_posts()
+        self._get_from_posts_details()
+        self._get_from_personal_number()
+        print(self.Data)
         return self.Data
 
-
+class GetToken:
+    def __init__(self, data_base_connection: sqlite3.connect):
+        self.DB_Conn = data_base_connection
+        self.Token = []
 if __name__ == '__main__':
     DATABASE = 'posts.db'
     CONNECTION_DB = sqlite3.connect(DATABASE)
