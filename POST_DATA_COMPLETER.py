721ae77b89fd42144c88cd1687ea57a1b7fdfb2d
mahdi0shamlou <shamlouprogramer@gmail.com>
2024-08-12 14:59:55 -0400

    create get_data class and this structer

--- POST_DATA_COMPLETER.py
+++ POST_DATA_COMPLETER.py
@@ -1,1 +1,27 @@
-import sqlite3
+from DataBase_manager import *
+
+
+class GetData:
+    """
+        this class get all data of one token
+    """
+    def __init__(self, token):
+        self.Token = token
+        self.Data = []
+
+    def _get_from_posts(self):
+        pass
+
+    def _get_from_posts_details(self):
+        pass
+
+    def _get_from_personal_number(self):
+        pass
+
+    def get_data(self):
+        return self.Data
+
+
+if __name__ == '__main__':
+    get_data_obj = GetData('token')
+    data = get_data_obj.get_data()
