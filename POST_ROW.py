c41ea1955e2ce346c56495400321beb587db5a2e
mahdi0shamlou <shamlouprogramer@gmail.com>
2024-08-13 15:11:41 -0400

    add a comment in post row and that show types of category divar can accept for buy and sell edary

--- POST_ROW.py
+++ POST_ROW.py
@@ -57,8 +57,10 @@ class Application:
 if __name__ == '__main__':
     URL = 'https://api.divar.ir/v8/postlist/w/search'
     DATA = '{"city_ids":["1"],"source_view":"FILTER","search_data":{"form_data":{"data":{"business-type":{"str":{"value":"personal"}},"sort":{"str":{"value":"sort_date"}},"category":{"str":{"value":"residential-sell"}}}}}}'
-    # DATA we can change category for sell in these type -> apartment-sell , house-villa-sell , plot-old
-    # DATA we can change categort for rent in these type -> allofthem (residential-rent) , apartment-rent , house-villa-rent
+    # DATA we can change category for sell in these type -> apartment-sell , house-villa-sell , plot-old : فروش مسکونی
+    # DATA we can change categort for rent in these type -> all of them (residential-rent) , apartment-rent , house-villa-rent : اجاره مسکونی
+    # DATA we can change categort for rent in these type -> all of them(commercial-sell) , office-sell , shop-sell , industry-agriculture-business-sell : فروش اداری
+    # DATA we can change categort for rent in these type -> all of them(commercial-rent) , office-rent , shop-rent , industry-agriculture-business-rent : اجاره اداری
     DB_FILENAME = 'posts.db'
     app = Application(URL, DATA, DB_FILENAME)
     dbs = DatabaseManager(DB_FILENAME)
