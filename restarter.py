6b177e0932ce7290bda50e859608b32bf35a0fcc
mahdi0shamlou <shamlouprogramer@gmail.com>
2024-08-16 13:34:05 -0400

    add a service to restart

--- restarter.py
+++ restarter.py
@@ -4,5 +4,6 @@ while True:
         print('Restarting Divar scraper')
         os.system("sudo systemctl restart Restart_POST_ROW_service.service")
         os.system("sudo systemctl restart Restart_POST_DETAILS_service.service")
+        os.system("sudo systemctl restart Restart_PERSONAL_NUMBER_service.service")
         print('Restarted Divar scraper')
         time.sleep(3000)
