8ac7216b0a48eab5225d3181b4cfe7950a4650e9
mahdi0shamlou <shamlouprogramer@gmail.com>
2024-08-20 15:43:05 -0400

    cleaning in SharpiMelk sender

--- SharpiMelk_Sender.py
+++ SharpiMelk_Sender.py
@@ -16,7 +16,20 @@ class InsertDataSharpiMelk:
             database='SharpiMelk',
             port=3306
         )
-        param = (posts['token'], posts['phone'], posts['city'], posts['city_text'], 0, posts['mahal_text'], posts['types'], posts['title'], posts['price'], posts['meter'], posts['desck'], str(posts['map']), str(posts['LIST_DATA']))
+        param = (posts['token'],
+                 posts['phone'],
+                 posts['city'],
+                 posts['city_text'],
+                 0,
+                 posts['mahal_text'],
+                 posts['types'],
+                 posts['title'],
+                 posts['price'],
+                 posts['meter'],
+                 posts['desck'],
+                 str(posts['map']),
+                 str(posts['LIST_DATA'])
+                 )
         query = f"""INSERT INTO PostFileSell (token, `number`, city, city_text, mahal, mahal_text, `type`, title, price, meter, desck, `map`, details) VALUES{param};"""
         cursor = connection.cursor()
         print(cursor.execute(query))
@@ -51,7 +64,11 @@ class GetDataFull:
         self.Data_full['title'] = posts[0][2]
         self.Data_full['mahal_text'] = posts[0][3]
 
-
+    def _get_from_personal_number(self):
+        cursor = self.DB_Conn.cursor()
+        cursor.execute('SELECT * FROM personal_number WHERE token = ?', (self.Token,))
+        posts = cursor.fetchall()
+        self.Data_full['phone'] = posts[0][3]
 
     def _get_from_posts_details(self):
         cursor = self.DB_Conn.cursor()
@@ -73,29 +90,16 @@ class GetDataFull:
                             if x['title'] == 'متراژ':
                                 self.Data_full['meter'] = int(x['value'])
                             if x['title'] == 'ساخت':
-                                #self.Data_full['make'] = int(x['value'])
+                                # self.Data_full['make'] = int(x['value'])
                                 pass
                             if x['title'] == 'اتاق':
                                 self.Data_full['otagh'] = int(x['value'])
 
                             print(x)
 
-
-
-
-            #print(i)
-
+            # print(i)
 
         self.Data_full['desck'] = posts[0][2]
-
-
-
-    def _get_from_personal_number(self):
-        cursor = self.DB_Conn.cursor()
-        cursor.execute('SELECT * FROM personal_number WHERE token = ?', (self.Token,))
-        posts = cursor.fetchall()
-        self.Data_full['phone'] = posts[0][3]
-
 
     def _check(self):
         cursor = self.DB_Conn.cursor()
@@ -116,31 +120,26 @@ class GetDataFull:
     def get_data(self):
         self.db_manager = DatabaseManager('posts.db')
         x = self._check()
-
         if x == 1:
-            print('sell')
+            print(f'\t this a sell file')
             self.Data_full['types'] = 1
             self._get_from_posts()
             self._get_from_personal_number()
             self._get_from_posts_details()
-            for (i,t) in self.Data_full.items():
-                print(i)
             InsertDataSharpiMelk.inser_data_sell(self.Data_full)
             self.db_manager.update_number_personal_for_post_sender(((self.Token,)))
         elif x == 2:
             print('rent')
             self.db_manager.update_number_personal_for_post_sender(((self.Token,)))
         elif x == 3:
-            print('sell tejary')
+            print(f'\t this a sell tejaryi file')
             self.Data_full['types'] = 3
             self._get_from_posts()
             self._get_from_personal_number()
             self._get_from_posts_details()
-            for (i,t) in self.Data_full.items():
-                print(i)
             InsertDataSharpiMelk.inser_data_sell(self.Data_full)
             self.db_manager.update_number_personal_for_post_sender(((self.Token,)))
-            #self.db_manager.update_number_personal_for_post_sender(((self.Token,)))
+            
         elif x == 4:
             print('rent tejary')
             self.db_manager.update_number_personal_for_post_sender(((self.Token,)))
@@ -166,7 +165,6 @@ if __name__ == "__main__":
     app = Application(DB_FILENAME)
     DATABASE = 'posts.db'
     CONNECTION_DB = sqlite3.connect(DATABASE)
-
     while True:
         try:
             print('Start of geting detials of service')
