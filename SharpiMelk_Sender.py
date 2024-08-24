41ec557a62e2a7c832a8c1cc82c7c34b32a9a2b7
mahdi0shamlou <shamlouprogramer@gmail.com>
2024-08-24 04:57:49 -0400

    create rent sharpi sender

--- SharpiMelk_Sender.py
+++ SharpiMelk_Sender.py
@@ -41,6 +41,42 @@ class InsertDataSharpiMelk:
         cursor = connection.cursor()
         print(cursor.execute(query))
         connection.commit()
+    @staticmethod
+    def inser_data_rent(posts):
+        print(len(posts))
+        connection = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='ya mahdi',
+            database='SharpiMelk',
+            port=3306
+        )
+
+        param = (posts['token'],
+                 posts['phone'],
+                 posts['city'],
+                 posts['city_text'],
+                 posts['mahal'],
+                 posts['mahal_text'],
+                 posts['types'],
+                 posts['title'],
+                 posts['credit'],
+                 posts['rent'],
+                 posts['meter'],
+                 posts['desck'],
+                 str(posts['map']),
+                 str(posts['LIST_DATA']),
+                 posts['GROUP_INFO_ROW'],
+                 posts['UNEXPANDABLE_ROW'],
+                 posts['GROUP_FEATURE_ROW'],
+                 posts['GROUP_FEATURE_ROW_items'],
+                 posts['GROUP_FEATURE_ROW_more_details'],
+                 posts['Images']
+                 )
+        query = f"""INSERT INTO PostFileRent (token, `number`, city, city_text, mahal, mahal_text, `type`, title, price, rent, meter, desck, `map`, details, GROUP_INFO_ROW, UNEXPANDABLE_ROW, GROUP_FEATURE_ROW, GROUP_FEATURE_ROW_items, GROUP_FEATURE_ROW_more_details, Images) VALUES{param};"""
+        cursor = connection.cursor()
+        print(cursor.execute(query))
+        connection.commit()
 
 
 class GetDataFull:
@@ -95,7 +131,6 @@ class GetDataFull:
         cursor.execute('SELECT * FROM posts_details_personal WHERE token = ?', (self.Token,))
         posts = cursor.fetchall()
         full_data = json.loads(posts[0][3])
-
         self.Data_full['price'] = full_data['webengage']['price']
         self.Data_full['map'] = ''
         self.Data_full['meter'] = 0
@@ -143,7 +178,8 @@ class GetDataFull:
                     if z['widget_type'] == 'GROUP_FEATURE_ROW':
                         if 'items' in z['data']:
                             for i in z['data']['items']:
-                                self.Data_full['GROUP_FEATURE_ROW_items'].append({i['icon']['icon_name']: i['available']})
+                                if 'available' in i:
+                                    self.Data_full['GROUP_FEATURE_ROW_items'].append({i['icon']['icon_name']: i['available']})
 
 
 
@@ -157,11 +193,6 @@ class GetDataFull:
                                     self.Data_full['GROUP_FEATURE_ROW'].append({x['data']['title']: x['data']['value']})
                                 if x['widget_type'] == 'FEATURE_ROW':
                                     self.Data_full['GROUP_FEATURE_ROW_more_details'].append(x['data']['title'])
-
-
-
-
-            # print(i)
         print(self.Data_full['GROUP_INFO_ROW'])
         self.Data_full['GROUP_INFO_ROW'] = str(self.Data_full['GROUP_INFO_ROW'])
         self.Data_full['GROUP_INFO_ROW'] = self.Data_full['GROUP_INFO_ROW'].replace("'", '"')
@@ -183,6 +214,95 @@ class GetDataFull:
 
         self.Data_full['desck'] = posts[0][2]
 
+
+    def _get_from_posts_details_rent(self):
+        cursor = self.DB_Conn.cursor()
+        cursor.execute('SELECT * FROM posts_details_personal WHERE token = ?', (self.Token,))
+        posts = cursor.fetchall()
+        full_data = json.loads(posts[0][3])
+        self.Data_full['credit'] = full_data['webengage']['credit']
+        self.Data_full['rent'] = full_data['webengage']['rent']
+        self.Data_full['map'] = ''
+        self.Data_full['meter'] = 0
+        self.Data_full['otagh'] = 0
+        self.Data_full['make'] = 0
+        self.Data_full['UNEXPANDABLE_ROW'] = []
+        self.Data_full['GROUP_INFO_ROW'] = []
+        self.Data_full['GROUP_FEATURE_ROW'] = []
+        self.Data_full['GROUP_FEATURE_ROW_items'] = []
+        self.Data_full['GROUP_FEATURE_ROW_more_details'] = []
+        self.Data_full['Images'] = []
+        for i in full_data['sections']:
+            if i['section_name'] == 'MAP':
+                self.Data_full['map'] = i
+            #---------------------------------------------------
+            elif i['section_name'] == 'IMAGE':
+                for z in i['widgets']:
+                    if z['widget_type'] == 'IMAGE_CAROUSEL':
+                        for x in z['data']['items']:
+                            self.Data_full['Images'].append(x['image']['url'])
+            # ---------------------------------------------------
+            elif i['section_name'] == 'LIST_DATA':
+                self.Data_full['LIST_DATA'] = i
+                for z in i['widgets']:
+                    if z['widget_type'] == 'GROUP_INFO_ROW':
+                        for x in z['data']['items']:
+                            self.Data_full['GROUP_INFO_ROW'].append({x['title']:x['value']})
+                            if x['title'] == 'متراژ':
+                                self.Data_full['meter'] = int(x['value'])
+                            if x['title'] == 'ساخت':
+                                try:
+                                    self.Data_full['make'] = int(x['value'])
+                                except:
+                                    self.Data_full['make'] = x['value']
+
+                            if x['title'] == 'اتاق':
+                                try:
+                                    self.Data_full['otagh'] = int(x['value'])
+                                except:
+                                    self.Data_full['otagh'] = x['value']
+
+                            print(x)
+                    if z['widget_type'] == 'UNEXPANDABLE_ROW':
+                        self.Data_full['UNEXPANDABLE_ROW'].append({z['data']['title']:z['data']['value']})
+                    if z['widget_type'] == 'GROUP_FEATURE_ROW':
+                        if 'items' in z['data']:
+                            for i in z['data']['items']:
+                                if 'available' in i:
+                                    self.Data_full['GROUP_FEATURE_ROW_items'].append({i['icon']['icon_name']: i['available']})
+
+
+
+
+                        if 'action' in z['data']:
+                            print(z['data']['action']['payload']['modal_page']['widget_list'])
+                            datas = z['data']['action']['payload']['modal_page']['widget_list']
+                            for x in datas:
+                                print(x)
+                                if x['widget_type'] == 'UNEXPANDABLE_ROW':
+                                    self.Data_full['GROUP_FEATURE_ROW'].append({x['data']['title']: x['data']['value']})
+                                if x['widget_type'] == 'FEATURE_ROW':
+                                    self.Data_full['GROUP_FEATURE_ROW_more_details'].append(x['data']['title'])
+        print(self.Data_full['GROUP_INFO_ROW'])
+        self.Data_full['GROUP_INFO_ROW'] = str(self.Data_full['GROUP_INFO_ROW'])
+        self.Data_full['GROUP_INFO_ROW'] = self.Data_full['GROUP_INFO_ROW'].replace("'", '"')
+        print(self.Data_full['UNEXPANDABLE_ROW'])
+        self.Data_full['UNEXPANDABLE_ROW'] = str(self.Data_full['UNEXPANDABLE_ROW'])
+        self.Data_full['UNEXPANDABLE_ROW'] = self.Data_full['UNEXPANDABLE_ROW'].replace("'", '"')
+        print(self.Data_full['GROUP_FEATURE_ROW'])
+        self.Data_full['GROUP_FEATURE_ROW'] = str(self.Data_full['GROUP_FEATURE_ROW'])
+        self.Data_full['GROUP_FEATURE_ROW'] = self.Data_full['GROUP_FEATURE_ROW'].replace("'", '"')
+        print(self.Data_full['GROUP_FEATURE_ROW_items'])
+        self.Data_full['GROUP_FEATURE_ROW_items'] = str(self.Data_full['GROUP_FEATURE_ROW_items'])
+        self.Data_full['GROUP_FEATURE_ROW_items'] = self.Data_full['GROUP_FEATURE_ROW_items'].replace("'", '"')
+        print(self.Data_full['GROUP_FEATURE_ROW_more_details'])
+        self.Data_full['GROUP_FEATURE_ROW_more_details'] = str(self.Data_full['GROUP_FEATURE_ROW_more_details'])
+        self.Data_full['GROUP_FEATURE_ROW_more_details'] = self.Data_full['GROUP_FEATURE_ROW_more_details'].replace("'", '"')
+        print(self.Data_full['Images'])
+        self.Data_full['Images'] = str(self.Data_full['Images'])
+        self.Data_full['Images'] = self.Data_full['Images'].replace("'",'"')
+
+        self.Data_full['desck'] = posts[0][2]
     def _check(self):
         cursor = self.DB_Conn.cursor()
         cursor.execute('SELECT * FROM posts_details_personal WHERE token = ?', (self.Token,))
@@ -210,32 +330,51 @@ class GetDataFull:
                 self._get_from_posts()
                 self._get_from_personal_number()
                 self._get_from_posts_details()
-
                 InsertDataSharpiMelk.inser_data_sell(self.Data_full)
                 self.db_manager.update_token_for_sharpi_melk(((self.Token,)))
             elif x == 2:
-                print('rent')
+                print(f'\t this a rent file')
+                self.Data_full['types'] = 2
+                self._get_from_posts()
+                self._get_from_personal_number()
+                self._get_from_posts_details_rent()
+                InsertDataSharpiMelk.inser_data_rent(self.Data_full)
                 self.db_manager.update_token_for_sharpi_melk(((self.Token,)))
+                print('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+
+
             elif x == 3:
                 print(f'\t this a sell tejaryi file')
                 self.Data_full['types'] = 3
                 self._get_from_posts()
                 self._get_from_personal_number()
                 self._get_from_posts_details()
-
                 InsertDataSharpiMelk.inser_data_sell(self.Data_full)
                 self.db_manager.update_token_for_sharpi_melk(((self.Token,)))
 
             elif x == 4:
-                print('rent tejary')
+                print(f'\t this a rent tejary file')
+                self.Data_full['types'] = 4
+                self._get_from_posts()
+                self._get_from_personal_number()
+                self._get_from_posts_details_rent()
+                InsertDataSharpiMelk.inser_data_rent(self.Data_full)
                 self.db_manager.update_token_for_sharpi_melk(((self.Token,)))
+                print('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
             else:
                 print('none')
                 self.db_manager.update_token_for_sharpi_melk(((self.Token,)))
+
+
         except ValueError as e:
+            print('Errrrrrrrrrrrrrrrrrrrrrrrrorrrrrrrrrrrrrrrrrrrrrr')
+            print(e)
             time.sleep(20)
-        except:
+        except Exception as e:
+            print('Errrrrrrrrrrrrrrrrrrrrrrrrorrrrrrrrrrrrrrrrrrrrrr')
+            print(e)
             self.db_manager.update_token_for_sharpi_melk(((self.Token,)))
+
 
 
 
