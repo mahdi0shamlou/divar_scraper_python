68ebd6af1559d768f16764ea8448e5070538ea23
mahdi0shamlou <shamlouprogramer@gmail.com>
2024-09-29 16:13:57 -0400

    create class app

--- New_Post_Sender.py
+++ New_Post_Sender.py
@@ -6,8 +6,28 @@ import mysql.connector
 
 
 
+class Application:
+    def __init__(self, db_filename, list_category):
+        self.db_manager = DatabaseManager(db_filename)
+        self.list_category = list_category
+
+    def run(self, CONNECTION_DB: sqlite3.connect):
+        pass
 
 
 if __name__ == "__main__":
     DB_FILENAME = 'posts.db'
-    objct_database = DatabaseManager(DB_FILENAME)
+    list_category = []
+
+    CONNECTION_DB = sqlite3.connect(DB_FILENAME)
+    app = Application(DB_FILENAME, list_category)
+    while True:
+        try:
+            print('Start of sending post in service')
+            app.run(CONNECTION_DB)
+            print('End of sending post in of service')
+        except Exception as e:
+            print(f'this is Eception : {e}')
+        finally:
+            time.sleep(1)
+            pass
